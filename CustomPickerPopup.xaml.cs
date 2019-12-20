3df4b84a61524c2cee980d5577f9461f670fe1dd
sjong13 <36455317+sjong13@users.noreply.github.com>
2019-12-20 12:54:50 -0600

    Update CustomPickerPopup.xaml.cs
    removed dependencies on other controls

--- CustomPickerPopup.xaml.cs
+++ CustomPickerPopup.xaml.cs
@@ -1,8 +1,10 @@
-﻿using Quorum.FieldVisor.Mobile.Extensions;
+using Plugin.Shared.Models;
+using Quorum.FieldVisor.Mobile.Extensions;
 using Rg.Plugins.Popup.Pages;
 using Rg.Plugins.Popup.Services;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,49 +17,85 @@ namespace Quorum.FieldVisor.Mobile.Pages.Popups
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class CustomPickerPopup : PopupPage
     {
-        public FloatLabeledEntry FLE;
+        private TaskCompletionSource<string> taskCompletion;
         public List<PickerItem> Items;
-        public CustomPickerPopup(List<string> items, FloatLabeledEntry fle = null)
+        public CustomPickerPopup(List<string> items)
         {
-            FLE = fle;
-            if (FLE == null)
-            {
-                FLE = new FloatLabeledEntry();
-            }
-            FLE.UnFocusEntry();
             Items = new List<PickerItem>();
 
             foreach (var item in items)
             {
-                Items.Add(new PickerItem()
+                if (item != null)
                 {
-                    Value = item
-                });
+                    Items.Add(new PickerItem()
+                    {
+                        Value = item
+                    });
+                }
+
             }
             InitializeComponent();
             mainList.ItemsSource = Items;
+            
             int wrappedItems = 0;
 
             foreach (var item in Items)
             {
-                if ((item.Value.Length > 27))
+                if ((item.Value.Length > 30))
                     wrappedItems++;
             }
-            mainList.HeightRequest = (Items.Count() + wrappedItems * 0.5) > 9 ? 400 : 44 * (Items.Count() + wrappedItems * 0.5);
+            mainList.HeightRequest = (Items.Count() + wrappedItems * 0.5) > 9 ? 463.5 : 51.5 * (Items.Count() + wrappedItems * 0.5);
+        }
+
+        public static async Task<string> GetSelectedPickerItem(object obj = null)
+        {
+            List<string> pickerItems = new List<string>();
+         
+            if (obj is IEnumerable<string>)
+            {
+                pickerItems = ((IEnumerable<string>)obj).ToList();
+            }
+            else if (obj is IEnumerable<SelectListItem>)
+            {
+                foreach (var item in (IEnumerable<SelectListItem>)obj)
+                {
+                    pickerItems.Add(item.Name);
+                }
+            }
+            
+
+            CustomPickerPopup pop = new CustomPickerPopup(pickerItems);
+
+            var result = await pop.GetSelectedPickerItemInternal(pickerItems);
+
+            return result;
+        }
+
+        private async Task<string> GetSelectedPickerItemInternal(List<string> pickerItems)
+        {
+            taskCompletion = new TaskCompletionSource<string>();
+
+            await PopupNavigation.Instance.PushAsync(this);
+
+            return await taskCompletion.Task;
         }
 
         private async void MainList_ItemSelected(object sender, SelectedItemChangedEventArgs e)
         {
             PickerItem item = e.SelectedItem as PickerItem;
-            FLE.Text = item.Value.ToString();
-            FLE.SelectedIndex = Items.IndexOf(Items.Where(x=>x.Value == item.Value.ToString()).FirstOrDefault());
-            FLE.UnFocusEntry();
+
+            if (taskCompletion != null)
+            {
+                taskCompletion.TrySetResult(item.Value.ToString());
+
+                taskCompletion = null;
+            }
             await PopupNavigation.Instance.PopAsync();
         }
 
-        private void PopupPage_Disappearing(object sender, EventArgs e)
+        private async void BackgroundTapped(object sender, EventArgs e)
         {
-            FLE.UnFocusEntry();
+            await PopupNavigation.Instance.PopAsync();
         }
     }
 
